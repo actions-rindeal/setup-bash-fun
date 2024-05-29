2c96814817e121bd6d56be296e51891d1e503c19
rindeal <dev.rindeal@gmail.com>
2024-05-30 00:32:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -260,6 +260,17 @@ class core {
     }
     this.#issueFileCommand('STATE', this.#prepareKeyValueMessage(name, value))
   }
+
+  static #issueFileCommand(command, message) {
+    const filePath = process.env[`GITHUB_${command}`]
+    if (!filePath) {
+      throw new Error(`Unable to find environment variable for file command ${command}`)
+    }
+    if (!fs.existsSync(filePath)) {
+      throw new Error(`Missing file at path: ${filePath}`)
+    }
+    fs.appendFileSync(filePath, `${this.#toCommandValue(message)}${os.EOL}`, { encoding: 'utf8' })
+  }
 }
 
 
