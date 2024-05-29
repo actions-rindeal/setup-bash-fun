8f0fb2316c8769e33bdc7dfcffd7e59d9e5a10ca
rindeal <dev.rindeal@gmail.com>
2024-05-30 01:16:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const SRC_BASH_FUN_PATH = 'fun.sh'
 const DEST_DEFAULT = `~/${SRC_BASH_FUN_PATH}`
 
 
-//region /////////////////////  Lightweight portable `@actions/core` class  /////////////////////////////////////////
+//region /////////////////////  Super Feathertweight `@actions/core` class  /////////////////////////////////////////
 
 /**
  * @class
@@ -41,7 +41,6 @@ class AnnotationProperties {
     this.endColumn = props.endColumn
   }
 }
-
 /**
  * @class Core
  * @description This class provides utility methods for handling environment variables, issuing commands, logging, and managing output groups. It is designed to be used in a Node.js environment.
@@ -60,215 +59,119 @@ class Core {
    */
   static getInput(name, required) {
     const val = process.env[`INPUT_${name.replace(/-/g, '_').toUpperCase()}`] || ''
-    if (required && !val) {
-      throw new Error(`Input required and not supplied: ${name}`)
-    }
+    if (required && !val) { throw new Error(`Input required and not supplied: ${name}`) }
     return val.trim()
   }
-
   /**
    * Checks if the runner is in debug mode.
    * @returns {boolean} Whether the runner is in debug mode.
    */
-  static isDebug() {
-    return process.env['RUNNER_DEBUG'] === '1'
-  }
-
+  static isDebug() { return process.env['RUNNER_DEBUG'] === '1' }
   /**
    * Issues a debug command.
    * @param {string} message - The debug message.
    */
-  static debug(message) {
-    this.#issueCommand('debug', {}, message)
-  }
-
+  static debug(message) { this.#issueCommand('debug', {}, message) }
   /**
    * Sets the action status to failed.
    * @param {string | Error} message - add error issue message
    */
-  static setFailed(message) {
-    process.exitCode = 1
-    this.error(message)
-  }
-
+  static setFailed(message) { process.exitCode = 1 ; this.error(message) }
   /**
    * Adds an error issue.
    * @param {string | Error} message - error issue message. Errors will be converted to string via toString()
    * @param {AnnotationProperties} properties - optional properties to add to the annotation.
    */
-  static error(message, properties = {}) {
-    this.#issueCommand('error', new AnnotationProperties(properties), message instanceof Error ? message.toString() : message)
-  }
-
+  static error(message, properties = {}) { this.#issueCommand('error', new AnnotationProperties(properties), message instanceof Error ? message.toString() : message) }
   /**
    * Adds a warning issue.
    * @param {string | Error} message - warning issue message. Errors will be converted to string via toString()
    * @param {AnnotationProperties} properties - optional properties to add to the annotation.
    */
-  static warning(message, properties = {}) {
-    this.#issueCommand('warning', new AnnotationProperties(properties), message instanceof Error ? message.toString() : message)
-  }
-
+  static warning(message, properties = {}) { this.#issueCommand('warning', new AnnotationProperties(properties), message instanceof Error ? message.toString() : message) }
   /**
    * Adds a notice issue.
    * @param {string | Error} message - notice issue message. Errors will be converted to string via toString()
    * @param {AnnotationProperties} properties - optional properties to add to the annotation.
    */
-  static notice(message, properties = {}) {
-    this.#issueCommand('notice', new AnnotationProperties(properties), message instanceof Error ? message.toString() : message)
-  }
-
+  static notice(message, properties = {}) { this.#issueCommand('notice', new AnnotationProperties(properties), message instanceof Error ? message.toString() : message) }
   /**
    * Writes info to log with console.log.
    * @param {string} message - info message
    */
-  static info(message) {
-    process.stdout.write(message + os.EOL)
-  }
-
+  static info(message) { process.stdout.write(message + os.EOL) }
   /**
    * Begins an output group.
    * @param {string} name - The name of the output group
    */
-  static startGroup(name) {
-    this.#issueCommand('group', {}, name)
-  }
-
+  static startGroup(name) { this.#issueCommand('group', {}, name) }
   /**
    * Ends an output group.
    */
-  static endGroup() {
-    this.#issueCommand('endgroup', {})
-  }
-
+  static endGroup() { this.#issueCommand('endgroup', {}) }
   /**
    * Wrap an asynchronous function call in a group.
    * @param {string} name - The name of the group
    * @param {() => Promise<T>} fn - The function to wrap in the group
    * @returns {Promise<T>}
    */
-  static async group(name, fn) {
-    this.startGroup(name)
-    try {
-      return await fn()
-    } finally {
-      this.endGroup()
-    }
-  }
-
+  static async group(name, fn) { this.startGroup(name) ; try { return await fn() } finally { this.endGroup() } }
   /**
    * Enables or disables the echoing of commands into stdout for the rest of the step.
    * @param {boolean} enabled
    */
-  static setCommandEcho(enabled) {
-    this.#issueCommand('echo', {}, enabled ? 'on' : 'off')
-  }
-  
+  static setCommandEcho(enabled) { this.#issueCommand('echo', {}, enabled ? 'on' : 'off') }
   /**
    * Gets the value of a state set by this action's main execution.
    * @param {string} name - name of the state to get
    * @returns {string}
    */
-  static getState(name) {
-    return process.env[`STATE_${name}`] || ''
-  }
-
+  static getState(name) { return process.env[`STATE_${name}`] || '' }
   /**
    * Converts the given path to the posix form.
    * @param {string} pth - Path to transform.
    * @returns {string} - Posix path.
    */
-  static toPosixPath(pth) {
-    return pth.replace(/[\\]/g, '/')
-  }
-
+  static toPosixPath(pth) { return pth.replace(/[\\]/g, '/') }
   /**
    * Converts the given path to the win32 form.
    * @param {string} pth - Path to transform.
    * @returns {string} - Win32 path.
    */
-  static toWin32Path(pth) {
-    return pth.replace(/[/]/g, '\\')
-  }
-
+  static toWin32Path(pth) { return pth.replace(/[/]/g, '\\') }
   /**
    * Converts the given path to a platform-specific path.
    * @param {string} pth - The path to platformize.
    * @returns {string} - The platform-specific path.
    */
-  static toPlatformPath(pth) {
-    return pth.replace(/[/\\]/g, path.sep)
-  }
-
+  static toPlatformPath(pth) { return pth.replace(/[/\\]/g, path.sep) }
   /**
    * Issues a command.
-   * @param {string} command - The command to issue.
-   * @param {Object} properties - The properties of the command.
-   * @param {string} message - The message of the command.
+   * @param {string} cmd - The command to issue.
+   * @param {Object} props - The properties of the command.
+   * @param {string} msg - The message of the command.
    */
-  static #issueCommand(command, properties, message) {
-    const cmdStr = `::${command} ${Object.entries(properties).map(([key, val]) => `${key}=${this.#escapeProperty(val)}`).join(',')}\n${this.#escapeData(message)}`
-    process.stdout.write(cmdStr + os.EOL)
-  }
-
-  /**
-   * Escapes data for a command.
-   * @param {string} s - The data to escape.
-   * @returns {string} The escaped data.
-   */
-  static #escapeData(s) {
-    return s.replace(/%/g, '%25').replace(/\r/g, '%0D').replace(/\n/g, '%0A')
-  }
-
-  /**
-   * Escapes a property for a command.
-   * @param {string} s - The property to escape.
-   * @returns {string} The escaped property.
-   */
-  static #escapeProperty(s) {
-    return s.replace(/%/g, '%25').replace(/\r/g, '%0D').replace(/\n/g, '%0A').replace(/:/g, '%3A').replace(/,/g, '%2C')
-  }
-
+  static #issueCommand(cmd, props, msg) { process.stdout.write(`::${cmd} ${Object.entries(props).map(([k, v]) => `${k}=${this.#escapeProperty(v)}`).join(',')}\n${this.#escapeData(msg)}` + os.EOL) }
+  static #escapeData(s) { return s.replace(/%/g, '%25').replace(/\r/g, '%0D').replace(/\n/g, '%0A') }
+  static #escapeProperty(s) { return s.replace(/%/g, '%25').replace(/\r/g, '%0D').replace(/\n/g, '%0A').replace(/:/g, '%3A').replace(/,/g, '%2C') }
   /**
    * Sanitizes an input into a string so it can be passed into issueCommand safely.
    * @param {any} input - input to sanitize into a string
    * @returns {string}
    */
-  static #toCommandValue(input) {
-    if (input === null || input === undefined) {
-      return ''
-    } else if (typeof input === 'string' || input instanceof String) {
-      return input
-    }
-    return JSON.stringify(input)
-  }
-
+  static #toCommandValue(input) { return input == null ? '' : (typeof input === 'string' || input instanceof String) ? input : JSON.stringify(input) }
   /**
    * Sets the name of the output to set.
    * @param {string} name - name of the output to set
    * @param {any} value - value to store. Non-string values will be converted to a string via JSON.stringify
    */
-  static setOutput(name, value) {
-    const filePath = process.env['GITHUB_OUTPUT']
-    if (!filePath) {
-      throw new Error('File path for setOutput not provided')
-    }
-    this.#issueFileCommand('OUTPUT', this.#prepareKeyValueMessage(name, value))
-  }
-
+  static setOutput(name, value) { this.#issueFileCommand('OUTPUT', this.#prepareKeyValueMessage(name, value)) }
   /**
    * Saves state for current action, the state can only be retrieved by this action's post job execution.
    * @param {string} name - name of the state to store
    * @param {any} value - value to store. Non-string values will be converted to a string via JSON.stringify
    */
-  static saveState(name, value) {
-    const filePath = process.env['GITHUB_STATE']
-    if (!filePath) {
-      throw new Error('File path for saveState not provided')
-    }
-    this.#issueFileCommand('STATE', this.#prepareKeyValueMessage(name, value))
-  }
-
+  static saveState(name, value) { this.#issueFileCommand('STATE', this.#prepareKeyValueMessage(name, value)) }
   /**
    * Issues a command to a file.
    * @param {string} command - The command to issue.
@@ -277,15 +180,10 @@ class Core {
    */
   static #issueFileCommand(command, message) {
     const filePath = process.env[`GITHUB_${command}`]
-    if (!filePath) {
-      throw new Error(`Unable to find environment variable for file command ${command}`)
-    }
-    if (!fs.existsSync(filePath)) {
-      throw new Error(`Missing file at path: ${filePath}`)
-    }
+    if (!filePath) { throw new Error(`Unable to find environment variable for file command ${command}`) }
+    if (!fs.existsSync(filePath)) { throw new Error(`Missing file at path: ${filePath}`) }
     fs.appendFileSync(filePath, `${this.#toCommandValue(message)}${os.EOL}`, { encoding: 'utf8' })
   }
-
   /**
    * Prepares a key-value message for a command.
    * @param {string} key - The key of the message.
@@ -297,7 +195,6 @@ class Core {
     const delimiter = `ghadelimiter_${this.uuidv4()}`
     return `${key}<<${delimiter}${os.EOL}${this.#toCommandValue(value)}${os.EOL}${delimiter}`
   }
-
   /**
    * Generates a version 4 UUID, a randomly generated UUID, as per RFC 4122.
    * @returns {string} A random UUID string.
