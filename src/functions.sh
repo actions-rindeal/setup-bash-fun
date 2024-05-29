4d6030fd5ff3e7a2c0877ecda8b32927e6aa3a59
rindeal <dev.rindeal@gmail.com>
2024-05-29 17:01:31 +0200

    Update functions.sh

--- src/functions.sh
+++ src/functions.sh
@@ -4,3 +4,5 @@
 ##     set-failed "No 'repo' input provided."
 ##     set-failed "Invalid 'repo' input." "Check 'repo' format: '%s'" "${REPO}"
 set-failed() { printf "::error title=$1::${2-$1}\n" "${@:2}" ; exit 123 ; }
+
+core:setFailed() { set-failed "${@}" ; }
