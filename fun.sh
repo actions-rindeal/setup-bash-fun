6dc0791cccf66133a82ebfffedff355c1c2e2b81
rindeal <dev.rindeal@gmail.com>
2024-08-09 21:16:32 +0200

    Delete fun.sh

--- fun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Usage:
-##     setFailedAndExit  "No 'repo' input provided."
-##     setFailedAndExit  "Invalid 'repo' input."  "Check 'repo' format: '%s'" "${REPO}"
-core:setFailedAndExit() { printf "::error title=$1::${2-$1}\n" "${@:3}" ; exit 1 ; }
